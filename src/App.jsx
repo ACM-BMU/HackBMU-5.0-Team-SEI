8480e47a316d683aa4a9b2bf8670d22573fb507c
lejt <92820402+lejt@users.noreply.github.com>
2022-02-17 14:38:44 -0800

    include react router and link imports

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+export default function App() {
+  return (
+    <main className="App">
+      App
+      <Routes>
+        <Route path="" element={< />} />
+      </Routes>
+    </main>
+  );
+}
